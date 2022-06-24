532e6a314649c63bf150f2f1e9f298cd1f5749e8
Ankit Kumar <107995311+Ankit7281@users.noreply.github.com>
2022-06-24 23:18:01 +0530

    Update assignment 61.c

--- assignment 61.c
+++ assignment 61.c
@@ -1,1 +1,12 @@
 
+#include<stdio.h>
+#include<conio.h>
+int main()
+  {
+  float m,n;
+  printf("enter two numbers");
+  scanf("%d %d",&m,&n);
+  printf("sum of %fand%f is %f",m,n,m+n);
+  getch();
+  }
+  
